16adb4cb558968322f8091431958d6f6f189f91a
Gustavo Guimarães da Silva <gustavo.guimaraes.da.silva@gmail.com>
2019-07-03 23:48:55 -0300

    Criar os formularios de cadastros e depoimentos e fazer a validação ao criar o cadastro

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from app.froms import CadastroForm
+from app.froms import DepoimentosForm
 
 # Create your views here.
 
 def mostrar_index(request):
     return render(request, 'index.html')
+
+def mostrar_cadastro(request):
+    formulario = CadastroForm(request.POST or None)
+    menssagem = ''
+    if formulario.is_valid():
+        formulario.save()
+        formulario = CadastroForm
+        menssagem = 'Cadastro realizado com sucesso'
+    return render(request, 'cadastro.html')
