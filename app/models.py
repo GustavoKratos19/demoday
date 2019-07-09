42e30f5d733de1e0245d95ce51d74582bd040445
Gustavo Guimarães da Silva <gustavo.guimaraes.da.silva@gmail.com>
2019-07-08 23:50:39 -0300

    Atribir valor padrão para as charfilds do banco de dados

--- app/models.py
+++ app/models.py
@@ -4,25 +4,25 @@ from django.db import models
 class CadastroDeUsuario(models.Model):
     genero_opcoes = [
         ('fem', 'Feminino'),
-        ('tra', 'Transsexual'),
+        ('mas', 'Masculino'),
         ('otr', 'Outros')
     ]
 
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
-    usuario = models.CharField(max_length=20)
-    data_nascimento = models.CharField(max_length=10)
-    cpf = models.CharField(max_length=14)
-    celular = models.CharField(max_length=14)
-    genero = models.CharField(max_length=3, choices=genero_opcoes)
-    senha = models.CharField(max_length=30)
+    nome = models.CharField(max_length=100, default='')
+    email = models.EmailField(max_length=254, default='')
+    usuario = models.CharField(max_length=20, default='')
+    data_nascimento = models.CharField(max_length=10, default='')
+    cpf = models.CharField(max_length=14, default='')
+    celular = models.CharField(max_length=14, default='')
+    genero = models.CharField(max_length=3, choices=genero_opcoes, default='')
+    senha = models.CharField(max_length=30, default='')
     foto = models.ImageField(upload_to='')
 
     def __str__(self):
         return self.nome
 
 class DepoimentosUsuario(models.Model):
-    titulo_depoimento = models.CharField(max_length=40)
+    titulo_depoimento = models.CharField(max_length=40, default='')
     depoimento = models.TextField(default='')
     foto_depoimento = models.ImageField(upload_to='')
     nome = models.ForeignKey(CadastroDeUsuario, on_delete=models.SET_DEFAULT, default='')
