6bf7c5c2de32889f30250dbfb8e2bdec56c93eb5
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2019-07-14 18:01:51 -0300

    criar animação na pagina inicial

--- app/static/js/pagina_inicial.js
+++ app/static/js/pagina_inicial.js
@@ -28,3 +28,46 @@ $('nav a').click(function(e){
       scrollTop: targetOffset - menuHeight
   }, 500);
 });
+
+// animação
+
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+(function(){
+let $target = $('.anime'),
+    animationClass = 'anime-start',
+    offset = $(window).height() * 3/4; 
+
+function animeScroll() {
+  let documentTop = $(document).scrollTop();
+
+  $target.each(function() {
+      let itemTop = $(this).offset().top - 750;
+      if (documentTop > itemTop) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+  })
+}
+
+animeScroll();
+
+$(document).scroll(debounce(function(){
+  animeScroll();
+}, 200));
+})();
