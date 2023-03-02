d8c4877fbc7b914ef9c19edf93f0494b6e18ab09
Gustavo Guimarães da Silva <40122765+GustavoKratos19@users.noreply.github.com>
2023-03-02 14:27:32 +0000

    atualização

--- manage.py
+++ manage.py
@@ -4,17 +4,22 @@ import os
 import sys
 
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+def jls_extract_def(django, core, management, exc, execute_from_command_line):
+    def main():
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)
+    return main
+
+
+main = jls_extract_def(django, core, management, exc, execute_from_command_line)
 
 
 if __name__ == '__main__':
---
+++ staticfiles/js/p_troca_frase.js
@@ -0,0 +1,12 @@
+    $(document).ready(function(){
+    let frases = ['incrível', 'forte', 'poderosa', 'confiante', 'empoderada', 'resistência' ];
+    let cont = 0;
+
+    $('#troca').text(frases[cont++]);
+    setInterval(function(){
+        $('#troca').fadeOut(function(){
+            if (cont >= frases.length) cont = 0;
+            $('#troca').text(frases[cont++]).fadeIn();
+        });
+    }, 3000);
+});
